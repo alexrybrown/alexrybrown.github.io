f1181d63ad67910d71575ef8d29493308300248a
alexrybrown <alexrybrown@gmail.com>
2017-05-03 15:45:26 -0600

    Added collisions and start and stop screens

--- Asteroids.elm
+++ Asteroids.elm
@@ -9,6 +9,7 @@ import Collage exposing (..)
 import Element exposing (..)
 -- Using html to create our program
 import Html exposing (..)
+import Html.Events exposing (..)
 -- Using keyboard to track key ups and key downs
 import Keyboard exposing (KeyCode)
 -- Using animation frame to track time diffs
@@ -30,22 +31,48 @@ main =
 
 -- MODEL
 
+generateInitialSeed = Random.generate InitialSeed (Random.int Random.minInt Random.maxInt)
+
 type alias Model =
-  { ship : Ship
-  -- , asteroids : List Asteroid
+  { menu : Menu
+  , ship : Ship
+  , asteroids : List Asteroid
+  , stars : List Star
   , keyDowns : Set KeyCode
   , windowHeight : Int
   , windowWidth : Int
+  , seed : Random.Seed
+  , message : String
   }
 
--- Any thing that can move around the screen.
+-- Any thing that is contained in a list and needs to be drawn
+type alias Drawable a =
+  { a |
+    x : Float,
+    y : Float,
+    shapeDetails : ShapeDetails
+  }
+
+-- Generic physics object
 type alias PhysicsObject a =
   { a |
     x : Float,
     y : Float,
     velocity : Float,
-    acceleration : Float,
     rotation : Int
+  }
+
+type alias ShapeDetails =
+  { shape : Float -> Float -> Shape
+  , shapeColor : Color
+  , shapeWidth : Int
+  , shapeHeight : Int
+  }
+
+type alias Menu =
+  { mainMenu : Bool
+  , paused : Bool
+  , endGame : Bool
   }
 
 type alias Ship =
@@ -56,6 +83,7 @@ type alias Ship =
   , rotation : Int
   , bullets : List Bullet
   , shotFired : Bool
+  , shapeDetails : ShapeDetails
   }
 
 type alias Bullet =
@@ -63,6 +91,7 @@ type alias Bullet =
   , y : Float
   , velocity : Float
   , rotation : Int
+  , shapeDetails : ShapeDetails
   }
 
 type alias Asteroid =
@@ -70,6 +99,57 @@ type alias Asteroid =
   , y : Float
   , velocity : Float
   , rotation : Int
+  , shapeDetails : ShapeDetails
+  }
+
+type alias Star =
+  { x : Float
+  , y : Float
+  , shapeDetails : ShapeDetails
+  }
+
+
+menuChoices : Menu
+menuChoices =
+  { mainMenu = True
+  , paused = False
+  , endGame = False
+  }
+
+
+spaceshipShape : ShapeDetails
+spaceshipShape =
+  { shape = oval
+  , shapeColor = white
+  , shapeWidth = 15
+  , shapeHeight = 15
+  }
+
+
+bulletShape : ShapeDetails
+bulletShape =
+  { shape = oval
+  , shapeColor = red
+  , shapeWidth = 5
+  , shapeHeight = 5
+  }
+
+
+asteroidShape : ShapeDetails
+asteroidShape =
+  { shape = oval
+  , shapeColor = grey
+  , shapeWidth = 60
+  , shapeHeight = 60
+  }
+
+
+starShape : ShapeDetails
+starShape =
+  { shape = oval
+  , shapeColor = yellow
+  , shapeWidth = 2
+  , shapeHeight = 2
   }
 
 
@@ -82,43 +162,29 @@ spaceship =
   , rotation = 0
   , bullets = []
   , shotFired = False
+  , shapeDetails = spaceshipShape
   }
 
 
-model : Model
-model =
-  { ship = spaceship
-  -- , asteroids = newAsteroids 4 []
+initialModel : Model
+initialModel =
+  { menu = menuChoices
+  , ship = spaceship
+  , asteroids = []
+  , stars = []
   , keyDowns = Set.empty
   , windowHeight = 500
   , windowWidth = 500
+  , seed = Random.initialSeed 0
+  , message = ""
   }
-
-
--- newAsteroids : Int -> List Asteroid -> List Asteroid
--- newAsteroids num acc =
---   if num == 0 then
---     acc
---   else
---     let
---       (newX, seedX) = (Random.generate (Random.int -250 250) (Random.initialSeed 0))
---       (newY, seedY) = (Random.generate (Random.int -250 250) (Random.initialSeed 0))
---       (newVelocity, seedVelocity) = (Random.generate (Random.float 0 0.1) (Random.initialSeed 0))
---       (newRotation, seedRotation) = (Random.generate (Random.int 0 359) (Random.initialSeed 0))
---     in
---       newAsteroids (num - 1)
---       ({ x = newX
---        , y = newY
---        , velocity = newVelocity
---        , rotation = newRotation
---        } :: acc)
 
 
 -- INIT
 
 init : (Model, Cmd Msg)
 init =
-  (model, Cmd.none)
+  (initialModel, generateInitialSeed)
 
 -- UPDATE
 
@@ -127,11 +193,19 @@ type Msg =
   | KeyDown KeyCode
   | KeyUp KeyCode
   | KeyPresses KeyCode
+  | InitialSeed Int
+  | StartGame
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
+    InitialSeed val ->
+      ({ model | seed = Random.initialSeed val }, Cmd.none)
+
+    StartGame ->
+      (createStars 200 (createAsteroids 1 initialModel), Cmd.none)
+
     KeyDown keyCode ->
       ({ model | keyDowns = Set.insert keyCode model.keyDowns }, Cmd.none)
 
@@ -142,12 +216,76 @@ update msg model =
       (keyPress keyCode model, Cmd.none)
 
     Delta dt ->
-      (applyPhysics dt (checkKeys model), Cmd.none)
+      if not model.menu.mainMenu && not model.menu.paused && not model.menu.endGame then
+        (checkCollisions (applyPhysics dt (checkKeys model)), Cmd.none)
+      else
+        (model, Cmd.none)
+
+
+createStars : Int -> Model -> Model
+createStars num model =
+  if num == 0 then
+    let
+      { menu } = model
+      newMenu =
+        { menu |
+          mainMenu = False,
+          paused = False,
+          endGame = False}
+    in
+      { model | menu = newMenu }
+  else
+    let
+      (newX, seedX) = (Random.step (Random.float -250 250) model.seed)
+      (newY, seedY) = (Random.step (Random.float -250 250) seedX)
+      stars =
+        { x = newX
+         , y = newY
+         , shapeDetails = starShape
+         } :: model.stars
+    in
+      createStars (num - 1)
+        ({ model |
+            stars = stars,
+            seed = seedY })
+
+
+createAsteroids : Int -> Model -> Model
+createAsteroids num model =
+  if num == 0 then
+    model
+  else
+    let
+      (newX, seedX) = (Random.step (Random.float -250 250) model.seed)
+      (newY, seedY) = (Random.step (Random.float -250 250) seedX)
+      (newVelocity, seedVelocity) = (Random.step (Random.float 0 0.1) seedY)
+      (newRotation, seedRotation) = (Random.step (Random.int 0 359) seedVelocity)
+      asteroids =
+        { x = newX
+         , y = newY
+         , velocity = newVelocity
+         , rotation = newRotation
+         , shapeDetails = asteroidShape
+         } :: model.asteroids
+    in
+      createAsteroids (num - 1)
+        ({ model |
+            asteroids = asteroids,
+            seed = seedRotation })
 
 
 keyPress : KeyCode -> Model -> Model
 keyPress keyCode model =
   case keyCode of
+    -- Esc
+    -- If the esc key is pressed we pause or unpause the game
+    27 ->
+      let
+        { menu } = model
+        paused = not model.menu.paused
+      in
+        { model | menu = { menu | paused = paused } }
+
     -- Space
     -- If the space button has been pressed we can fire another shot.
     32 ->
@@ -231,27 +369,30 @@ checkKeysHelper model keyCodes =
 applyPhysics : Float -> Model -> Model
 applyPhysics dt model =
   let
-    { ship } = model
-    newShip = applyPhysicsShip dt ship
+    { ship, asteroids } = model
+    newShip = applyPhysicsShip dt ship model
     { bullets } = newShip
-    newBullets = applyPhysicsBullets model dt bullets
+    newBullets = (checkBulletOutOfBounds model (applyObjectPhysics model dt bullets) [])
+    newAsteroids = (checkAsteroidOutOfBounds model (applyObjectPhysics model dt asteroids) [])
   in
-    { model | ship = { newShip | bullets = newBullets } }
+    { model |
+      ship = { newShip | bullets = newBullets },
+      asteroids = newAsteroids }
 
 
-applyPhysicsShip : Float -> Ship -> Ship
-applyPhysicsShip dt ship =
+applyPhysicsShip : Float -> Ship -> Model -> Ship
+applyPhysicsShip dt ship model =
   let
     calculatedVelocity =
       ship.velocity + ship.acceleration * dt
 
     newVelocity =
-        if calculatedVelocity >= 0 && calculatedVelocity <= 0.2 then
+        if calculatedVelocity >= 0 && calculatedVelocity <= 0.15 then
           calculatedVelocity
         else if calculatedVelocity < 0 then
           0
         else
-          0.2
+          0.15
 
     calculatedXPosition =
       1/2 * ship.acceleration * dt * dt + cos (degrees (toFloat ship.rotation)) * newVelocity * dt + ship.x
@@ -282,22 +423,22 @@ applyPhysicsShip dt ship =
     }
 
 
-applyPhysicsBullets : Model -> Float -> List Bullet -> List Bullet
-applyPhysicsBullets model dt bullets =
-  List.map (applyBulletPhysics dt) (checkBulletOutOfBounds model bullets [])
+applyObjectPhysics : Model -> Float -> List (PhysicsObject a) -> List (PhysicsObject a)
+applyObjectPhysics model dt physObjs =
+  List.map (applyObjectPhysicsHelper dt) physObjs
 
 
-applyBulletPhysics : Float -> Bullet -> Bullet
-applyBulletPhysics dt bullet =
+applyObjectPhysicsHelper : Float -> PhysicsObject a -> PhysicsObject a
+applyObjectPhysicsHelper dt physObj =
   let
-    { velocity } = bullet
+    { velocity } = physObj
     calculatedXPosition =
-      cos (degrees (toFloat bullet.rotation)) * velocity * dt + bullet.x
+      cos (degrees (toFloat physObj.rotation)) * velocity * dt + physObj.x
 
     calculatedYPosition =
-      sin (degrees (toFloat bullet.rotation)) * velocity * dt + bullet.y
+      sin (degrees (toFloat physObj.rotation)) * velocity * dt + physObj.y
   in
-    { bullet
+    { physObj
     | x = calculatedXPosition
     , y = calculatedYPosition
     }
@@ -305,19 +446,38 @@ applyBulletPhysics dt bullet =
 
 checkBulletOutOfBounds : Model -> List Bullet -> List Bullet -> List Bullet
 checkBulletOutOfBounds model bullets acc =
-  if List.isEmpty bullets then
-    acc
-  else
-    let
-      bullet : Bullet
-      bullet = Maybe.withDefault badBullet (List.head bullets)
-      rest = Maybe.withDefault [] (List.tail bullets)
-    in
+  case bullets of
+    bullet::rest ->
       if bullet.x >= -(toFloat model.windowWidth) / 2 && bullet.x <= (toFloat model.windowWidth) / 2  &&
           bullet.y >= -(toFloat model.windowHeight) / 2 && bullet.y <= (toFloat model.windowHeight) / 2  then
         checkBulletOutOfBounds model rest (bullet :: acc)
       else
         checkBulletOutOfBounds model rest acc
+    [] -> acc
+
+
+checkAsteroidOutOfBounds : Model -> List Asteroid -> List Asteroid -> List Asteroid
+checkAsteroidOutOfBounds model asteroids acc =
+  case asteroids of
+    asteroid::rest ->
+      let
+        x =
+          if asteroid.x < -(toFloat model.windowWidth) / 2 then
+            (toFloat model.windowWidth) / 2
+          else if asteroid.x > (toFloat model.windowWidth) / 2 then
+            -(toFloat model.windowWidth) / 2
+          else
+            asteroid.x
+        y =
+          if asteroid.y < -(toFloat model.windowHeight) / 2 then
+            (toFloat model.windowHeight) / 2
+          else if asteroid.y > (toFloat model.windowHeight) / 2 then
+            -(toFloat model.windowHeight) / 2
+          else
+            asteroid.y
+      in
+        checkAsteroidOutOfBounds model rest ({ asteroid | x = x, y = y} :: acc)
+    [] -> acc
 
 
 updateAccelerationShip : Float -> Ship -> Ship
@@ -359,12 +519,84 @@ fireShot ship =
       , y = ship.y
       , velocity = 0.3
       , rotation = ship.rotation
+      , shapeDetails = bulletShape
       } :: bullets
   in
     { ship
     | bullets = newBullets
     , shotFired = True
     }
+
+
+-- Check collisions between records in our model
+checkCollisions : Model -> Model
+checkCollisions model =
+  let
+    { ship, menu } = model
+    (asteroids, bullets) = checkAsteroidBulletCollision model.asteroids model.ship.bullets []
+    (endGame, message) =
+      if checkShipCollision ship asteroids then
+        (True, "You Lose")
+      else if List.isEmpty asteroids then
+        (True, "You Win!")
+      else
+        (False, "")
+  in
+    { model
+    | asteroids = asteroids
+    , message = message
+    , menu = { menu | endGame = endGame }
+    , ship = { ship | bullets = bullets } }
+
+
+checkAsteroidBulletCollision : List Asteroid -> List Bullet -> List Asteroid -> (List Asteroid, List Bullet)
+checkAsteroidBulletCollision asteroids bullets asteroidsAcc =
+  case asteroids of
+    asteroid::rest ->
+      -- For a given asteroid check if any bullets are in it
+      let
+        (keepAsteroid, newBullets) = checkBulletsInAsteroid asteroid bullets []
+      in
+        if keepAsteroid then
+          checkAsteroidBulletCollision rest newBullets (asteroid :: asteroidsAcc)
+        else
+          checkAsteroidBulletCollision rest newBullets asteroidsAcc
+    [] ->
+      (asteroidsAcc, bullets)
+
+
+checkBulletsInAsteroid : Asteroid -> List Bullet -> List Bullet -> (Bool, List Bullet)
+checkBulletsInAsteroid asteroid bullets bulletsAcc =
+  case bullets of
+    bullet::rest ->
+      -- If this bullet is in the asteroid, return all the other bullets
+      if drawableInDrawable bullet asteroid then
+        (False, bulletsAcc ++ rest)
+      else
+        checkBulletsInAsteroid asteroid rest (bullet :: bulletsAcc)
+    [] ->
+      (True, bulletsAcc)
+
+
+checkShipCollision : Ship -> List Asteroid -> Bool
+checkShipCollision ship asteroids =
+  case asteroids of
+    asteroid::rest ->
+      if drawableInDrawable ship asteroid then
+        True
+      else
+        checkShipCollision ship rest
+    [] -> False
+
+
+-- Check to see if a given drawable is in another drawable
+drawableInDrawable : Drawable a -> Drawable b -> Bool
+drawableInDrawable drawableOne drawableTwo =
+  let
+    radius = ((toFloat drawableTwo.shapeDetails.shapeWidth) / 2)^2
+    distance = (drawableOne.x - drawableTwo.x)^2 + (drawableOne.y - drawableTwo.y)^2
+  in
+    distance <= radius
 
 
 -- VIEW
@@ -374,43 +606,41 @@ view : Model -> Html Msg
 view model =
   -- Html.text (toString model)
   let
-    { ship } = model
+    { ship, menu, asteroids, stars } = model
     { bullets } = ship
   in
-    toHtml <|
-      container model.windowWidth model.windowHeight middle <|
-        collage model.windowWidth model.windowHeight
-          ([ rect (toFloat model.windowWidth) (toFloat model.windowHeight)
-              |> filled (rgb 0 0 0)
-          , oval 15 15
-              |> filled white
-              |> move (ship.x, ship.y)
-          ] ++ (showBullets bullets []))
+    if menu.mainMenu then
+      div []
+        [ div [] [ Html.text "Asteroids" ]
+        , div []
+          [ button [ Html.Events.onClick StartGame ] [ Html.text "Play" ]]
+        ]
+    else if menu.endGame || menu.paused then
+      div []
+        [ div [] [ Html.text model.message ]
+        , div []
+          [ button [ Html.Events.onClick StartGame ] [ Html.text "Restart" ]]
+        ]
+    else
+      toHtml <|
+        container model.windowWidth model.windowHeight middle <|
+          collage model.windowWidth model.windowHeight
+            ([ rect (toFloat model.windowWidth) (toFloat model.windowHeight)
+                |> filled (rgb 0 0 0)]
+              ++ (showDrawables yellow (oval 2 2) stars [])
+              ++ (showDrawables red (oval 5 5) bullets [])
+              ++ [oval 15 15
+                  |> filled white
+                  |> move (ship.x, ship.y)]
+              ++ (showDrawables grey (oval 60 60) asteroids []))
 
 
-badBullet : Bullet
-badBullet =
-  { x = -1
-  , y = -1
-  , velocity = -1
-  , rotation = -1
-  }
-
-
-showBullets : List Bullet -> List Form -> List Form
-showBullets bullets acc =
-  if List.isEmpty bullets then
-    acc
-  else
-    let
-      bullet : Bullet
-      bullet = Maybe.withDefault badBullet (List.head bullets)
-      rest = Maybe.withDefault [] (List.tail bullets)
-    in
-      if not (bullet == badBullet) then
-        showBullets rest ((move (bullet.x, bullet.y) (filled red (oval 5 5))) :: acc)
-      else
-        showBullets rest acc
+showDrawables : Color -> Shape -> List (Drawable a) -> List Form -> List Form
+showDrawables shapeColor shape drawables acc =
+  case drawables of
+    drawable::rest ->
+      showDrawables shapeColor shape rest ((move (drawable.x, drawable.y) (filled shapeColor shape)) :: acc)
+    [] -> acc
 
 
 -- SUBSCRIPTIONS
